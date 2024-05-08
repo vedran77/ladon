bb8719d5a4970e80e318091983d843728be640d2
vedranvucic77 <vedran.vucic@codaxy.com>
2024-05-08 10:07:34 +0200

    remove commands

--- src/connection-manager/ExtendedConnection.ts
+++ src/connection-manager/ExtendedConnection.ts
@@ -2,5 +2,5 @@ import { TeamSpeak, TeamSpeakClient } from "ts3-nodejs-library";
 
 export interface ExtendedConnection extends TeamSpeak {
 	connectionId?: string;
-	registerCommand?(command: string, callback: (invoker: TeamSpeakClient, args: string[]) => Promise<void>): void;
+	//registerCommand?(command: string, callback: (invoker: TeamSpeakClient, args: string[]) => Promise<void>): void;
 }
